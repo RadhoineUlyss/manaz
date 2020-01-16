568abe800f4605888928d8c9ac3645a71df45700
RadhoineUlyss <43170329+RadhoineUlyss@users.noreply.github.com>
2020-01-16 17:02:38 +0100

    try to solve beug V1

--- application/libraries/dynamic_navbar.php
+++ application/libraries/dynamic_navbar.php
@@ -1,43 +1,43 @@
 <?php if (!defined('BASEPATH')) {
-    exit('No direct script access allowed');
+	exit('No direct script access allowed');
 }
 
 class Dynamic_navbar
 {
 
-    public function verification($page = '', $script='', $style='')
-    {
+	public function verification($page = '', $script = '', $style = '')
+	{
 
-        $view_data = [];
-        $CI        = &get_instance();
-        if ($page == '') {
-            $page = $CI->load->view('accueil_view', '', true);
-        }
+		$view_data = [];
+		$CI        = &get_instance();
+		if ($page == '') {
+			$page = $CI->load->view('accueil_view', '', true);
+		}
 
-        if ($CI->session->userdata('login_User')) {
-            $navbar     = $CI->load->view('Template/logged_button_inc_view', '', true);
-            $data       = [
-                'page'   => $page,
-                'navbar' => $navbar,
-                'script' => $script,
-                'style' => $style
-            ];
-            $data_merge = array_merge($data, $view_data);
-            $CI->load->view('Template/template', $data_merge);
-        } else {
-            $navbar     = $CI->load->view('Template/signin_button_inc_view', '', true); /* $navbar correspond aux boutons présents à droite de la navbar */
-            $formulaire = $CI->load->view('Template/signin_modal_inc_view', '', true);
-            $data = [
-                'page' => $page,
-                'navbar' => $navbar,
-                'script' => $script,
-                'style' => $style,
-                'formulaire' => $formulaire
-            ];
-            $data_merge = array_merge($data, $view_data);
-            $CI->load->view('Template/template', $data_merge);
-        }
-    }
+		if ($CI->session->userdata('login_User')) {
+			$navbar     = $CI->load->view('Template/logged_button_inc_view', '', true);
+			$data       = [
+				'page'   => $page,
+				'navbar' => $navbar,
+				'script' => $script,
+				'style'  => $style
+			];
+			$data_merge = array_merge($data, $view_data);
+			$CI->load->view('Template/template', $data_merge);
+		} else {
+			$navbar     = $CI->load->view('Template/signin_button_inc_view', '', true); /* $navbar correspond aux boutons présents à droite de la navbar */
+			$formulaire = $CI->load->view('Template/signin_modal_inc_view', '', true);
+			$data       = [
+				'page'       => $page,
+				'navbar'     => $navbar,
+				'script'     => $script,
+				'style'      => $style,
+				'formulaire' => $formulaire
+			];
+			$data_merge = array_merge($data, $view_data);
+			$CI->load->view('Template/template', $data_merge);
+		}
+	}
 }
 
 /* End of file Dynamic_navbar.php */
