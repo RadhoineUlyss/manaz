43e9a66ab4c8f2433583f1b3c75eb5fd9c943927
Zales13 <43754135+Zales13@users.noreply.github.com>
2020-01-31 17:27:13 +0100

    Navbar et burger pas finis

--- application/views/Template/template.php
+++ application/views/Template/template.php
@@ -15,7 +15,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 	<!-- Font -->
 	<link type="text/css" href="http://fonts.googleapis.com/css?family=Lato:300,700"/>
 	<!-- FavIcon -->
-	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url(); ?>assets/favicon/mannaz-12.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url(); ?>assets/favicon/logo-mannaz1.png">
 	<link rel="manifest" href="<?php echo base_url(); ?>assets/favicon/manifest.json">
 	<meta name="msapplication-TileColor" content="#ffffff">
 	<meta name="msapplication-TileImage" content="<?php echo base_url(); ?>assets/favicon/ms-icon-144x144.png">
@@ -30,90 +30,51 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 <!-- NE PAS TOUCHER EN HAUT -->
 
+
 <header>
+	
 	<!-- LA BARRE DE NAVIGATION DEBUT -->
 	<div id="navigation">
-
-</nav>
-		<ul>
-			<li><a href="<?php echo base_url('') ?>"><img id="logotop" src="<?php echo base_url(); ?>assets/img/mannaz-11.png"></a></li>
-			<li><a href="<?php echo base_url('') ?>"><a style="margin-top:20%; color:#FF8F71" href="">Accueil</a></li>
-			<li><a style="margin-right:9px; margin-top:9%;"href="">Développement personnel</a></li>
+		
+        <ul>
+			<li><a href="<?php echo base_url('') ?>"><a style="margin-top:10%;" href="">L'equilibre naît de l'action.</a></li>
 			<li><a style="margin-right:9px; margin-top:10%;"href="">Qu'est ce que Mannaz ?</a></li>
-			<li><a style="margin-right:9px; margin-top:15%;"href="">Mon approche</a></li>
-			<li><a style="margin-right:9px; margin-top:30%;"href="">Blog</a></li>
-			<li><a href="<?php echo base_url('') ?>"><img style="margin-top:21%; height: 15px; width: 15px;"id="i" src="<?php echo base_url(); ?>assets/img/i.svg"><img style="margin-top:21%;height: 15px; width: 15px; margin-left: 18px" id="f" src="<?php echo base_url(); ?>assets/img/f.svg"><img style="margin-top:21%;height: 23px; width: 25px; margin-left: 18px"id="in" src="<?php echo base_url(); ?>assets/img/in.svg"></a></li>
-			<li><input style="margin-top:20%;margin-left:25%;" id="connexion" type="button" value="Connexion">
+			<li><a href="<?php echo base_url('') ?>"><img id="logomid" src="<?php echo base_url(); ?>assets/img/logo-mannaz1.png"></a></li>	
+			<li><a style="margin-right:9px; margin-top:15%;"href="">Suivez-nous</a></li>
+			<li><a href="<?php echo base_url('') ?>"><img style="margin-top:10%; height: 15px; width: 15px;"id="i" src="<?php echo base_url(); ?>assets/img/i.svg"><img style="margin-top:10%;height: 15px; width: 15px; margin-left: 18px" id="f" src="<?php echo base_url(); ?>assets/img/f.svg"><img style="margin-top:10%;height: 23px; width: 25px; margin-left: 18px"id="in" src="<?php echo base_url(); ?>assets/img/in.svg"></a></li>
+			<li><input style="margin-top:15%;margin-left:25%;" id="connexion" type="button" value="Connexion">
 
 		</ul>
-	</div>
+		
 	
-
+	
 	<!-- LA BARRE DE NAVIGATION FIN -->
 
 </header>
-<body>
-<div id="video">
-	<video controls>
-    <source src="\assets\img\video.png"
-            type="video/webm">
-    <source src="\assets\img\video.png"
-            type="video/mp4">
-    Sorry, your browser doesn't support embedded videos.
-</video>
+<nav role="navigation">
+  <div id="menuToggle">
+    <input type="checkbox" />
 
-<div id="exx">
-	<div style="padding-left: 13%;">
-		<p>
-			<span style="font-size: 2em;">Votre coach </span>
-			<span style="color: blue;font-size: 2em;">Mannaz </span>
-			<span style="font-size: 2em;">2.0</span>
-		</p>
-		<p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+    <span></span>
+    <span></span>
+    <span></span>
+
+    <ul id="menu">
+	  <a href="#"><li>Accueil</li> </a>
+	  <br> </br>
+      <a href="#"><li>Développement personnel & professionnel</li></a>
+      <a href="#"><li>Qu'est ce que Mannaz ?</li></a>
+      <a href="#"><li>Mon approche</li></a>
+     </ul>
 	</div>
-	<div class="container">
- 	 	<div class="row">
-    		<div class="col-sm">
-      			<img style="width: 300px;height: 300px;" src="\assets\img\Daniela_Verdier.png" alt="Daniela_Verdier">
-   			</div>
-   		 	<div class="col-sm">
-      			One of three columns
-    		</div>
-  		</div>
-	</div>
-</div>
-<br>
-</div>
-<div id="thema">
-	<p id="thema_1">Différentes thématiques que nous pourrions aborder ensemble</p>
-	<p ip="thema_2">Survolez les illustrations</p>
-	<div id="thema_img">
-		<img id="i4"src="<?php echo base_url(); ?>assets/img/4.png" alt="homme qui baille">
-		<p>Vos émotions vous submergent facilemnt...</p>
-		<img id="i5"src="<?php echo base_url(); ?>assets/img/5.png" alt="homme qui baille">
-		<p>Atteint d'une maladie vous avez besoin de parler...</p>
-		<img id="i6"src="<?php echo base_url(); ?>assets/img/6.png" alt="homme qui baille">
-		<p>Vous êtes bleu et c'est difficile dans la vie de tous les jours...</p>
-		<img id="i7"src="<?php echo base_url(); ?>assets/img/7.png" alt="homme qui baille">
-	</div>
-</div>
+   </nav>
+<body>
+
+
 <!-- Footer -->
 
 			<!-- Grid row-->
-			<div class="row py-4 d-flex align-items-center">
-				<!-- Grid column -->
-
-				<!-- Grid column -->
-				<div class="col-md-6 col-lg-7 text-center text-md-right">
-
-					<!-- Facebook -->
-					<a class="fb-ic" href="#"> <i class="fa fa-facebook whiteicon mr-4"> </i> </a>
-					<!-- Twitter -->
-					<a class="tw-ic" href="#"> <i class="fa fa-twitter whiteicon mr-4"> </i> </a>
-					<!--Instagram-->
-					<a class="ins-ic" href="#"> <i class="fa fa-instagram whiteicon mr-4"> </i> </a>
-
-				</div>
+			
 				<!-- Grid column -->
 
 			</div>
@@ -129,29 +90,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 		<div class="row mt-3">
 
 			<!-- Grid column -->
-			<div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-
-				<!-- Content -->
-				<h6 class="text-uppercase font-weight-bold">Mannaz</h6>
-				<p>L'équilibre naît de l'action.</p>
-
-			</div>
-			<!-- Grid column -->
-			<div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-
-				<!-- Links -->
-				<h6 class="text-uppercase font-weight-bold">onglet 1</h6>
-				<p class="footerSecText">
-					<a href="<?php echo base_url('/sneakers') ?>">onglet 2</a>
-				</p>
-				<p class="footerSecText">
-					<a href="#!">onglet 3</a>
-				</p>
-				<p class="footerSecText">
-					<a href="#!">onglet 4</a>
-				</p>
-
-			</div>
+			
 			<!-- Grid column -->
 
 
